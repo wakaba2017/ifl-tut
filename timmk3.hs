5b14cac556c1a9b526d151edcdfb4a127341853d
wakaba2017 <32032690+wakaba2017@users.noreply.github.com>
2023-09-20 21:24:23 +0900

    Update timmk3.hs
    letrec対応 (Ex. 4.14)

--- timmk3.hs
+++ timmk3.hs
@@ -236,10 +236,12 @@ compileR (ELet recursion defs e) env d  -- Mark3で追加
       defs1 = map fst defs
       defs2 = map snd defs
       subFunc1 dd ee
-        | recursion == True = compileA ee env_ dd  -- letrec の場合
-        | otherwise         = compileA ee env  dd  -- let    の場合
+        | recursion == True = compileA ee env__ dd  -- letrec の場合
+        | otherwise         = compileA ee env   dd  -- let    の場合
       (dn, ams) = mapAccuml subFunc1 (d + length defs) defs2
-      env_ = (zip2 defs1 (map Arg [(d+1)..])) ++ env
+      env_  = (zip2 defs1 (map Arg [(d+1)..])) ++ env
+      -- env__ = (zip2 defs1 (map Code (map (replicate 1) (map Enter (map Arg [(d+1)..]))))) ++ env
+      env__ = (zip2 defs1 (map (Code . (replicate 1) . Enter . Arg) [(d+1)..])) ++ env
       (d_, is) = compileR e env_ dn
       subFunc2 n am = (n + 1, Move n am)
       (_, mvinstrs) = mapAccuml subFunc2 (d + 1) ams
@@ -296,6 +298,10 @@ compileB (ENum n) env d cont  -- Mark3で変更
 compileB e env d cont  -- Mark3で変更
   = (d_, Push (Code cont) : is)  -- Mark3で変更
     where (d_, is) = compileR e env d  -- Mark3で変更
+
+-- Mark2で追加
+mkIndMode :: Int -> TimAMode
+mkIndMode n = Code [Enter (Arg n)]
 ---------------------------------------
 -- プログラムのコンパイル (ここまで) --
 ---------------------------------------
@@ -600,6 +606,10 @@ ex_4_13 = "f x = letrec p = if (x==0) 1 q ; " ++
                        "q = if (x==0) p 2 " ++
                 "in p+q ; " ++
           "main = f 1"
+ex_4_13' = "f x = letrec a = b ; " ++
+                        "b = x " ++
+                  "in a ; " ++
+           "main = f 10"
 --------------------------------
 -- テストプログラム (ここまで) --
 --------------------------------
