a4f6097334a43e04d6566267e00ff71b8a8b9454
wakaba2017 <32032690+wakaba2017@users.noreply.github.com>
2024-10-13 16:27:25 +0900

    Update timmk4_7.hs
    ・コンパイラRスキーム修正 (図4.5の2番目の定義式(関数適用の引数がアトミックでない場合)について未対応だったことを修正。)
    ・コンパイラBスキーム修正 (図4.2の3番目の定義式(定数や算術式以外の場合)についてフレームスロットの再利用が未対応だったことを修正。)
    ・showCompiledCode関数修正 (コア言語プログラムをparseしてcompileした結果にshowSCDefnsを適用するよう修正。)
    ・コンパイラRスキーム、コンパイラBスキーム修正確認用テストプログラム追加。

--- timmk4_7.hs
+++ timmk4_7.hs
@@ -313,12 +313,25 @@ compileR (EAp (EAp (EAp (EVar "if") e0) e1) e2) env d  -- Mark3で変更
           d3 = max d1 d2
 compileR (EAp (EAp (EVar op) e1) e2) env d  -- Mark3で変更
   | op `elem` op_list = compileB (EAp (EAp (EVar op) e1) e2) env d [Return]  -- Mark3で変更
-  | otherwise         = (d2, Move (d + 1) am1 : Push (mkIndMode (d + 1)) : is)  -- Mark4で変更
+  | otherwise         = case e2 of
+                        EVar v -> (d1, Push am : is)  -- Mark4で変更
+                                  where am = compileA (EVar v) env
+                                        (d1, is) = compileR (EAp (EVar op) e1) env d
+                        ENum n -> (d1, Push am : is)  -- Mark4で変更
+                                  where am = compileA (ENum n) env
+                                        (d1, is) = compileR (EAp (EVar op) e1) env d
+                        _      -> (d2, Move (d + 1) am1 : Push (mkIndMode (d + 1)) : is)  -- Mark4で変更
+                                  where (d1, am1) = compileU e2 (d + 1) env (d + 1)  -- Mark4で変更
+                                        (d2, is)  = compileR (EAp (EVar op) e1) env d1  -- Mark4で変更
   where op_list = ["+", "-", "*", "/", "<", "<=", ">", ">=", "==", "~="]
-        (d1, am1) = compileU e2 (d + 1) env (d + 1)  -- Mark4で変更
-        (d2, is)  = compileR (EAp (EVar op) e1) env d1  -- Mark4で変更
 compileR (EAp (EVar "negate") e) env d  -- Mark3で変更
   = compileB (EAp (EVar "negate") e) env d [Return]  -- Mark3で変更
+compileR (EAp e (EVar v)) env d = (d1, Push am : is)  -- Mark4で変更
+                                  where am = compileA (EVar v) env
+                                        (d1, is) = compileR e env d
+compileR (EAp e (ENum n)) env d = (d1, Push am : is)  -- Mark4で変更
+                                  where am = compileA (ENum n) env
+                                        (d1, is) = compileR e env d
 compileR (EAp e1 e2) env d = (d2, Move (d + 1) am : Push (mkIndMode (d + 1)) : is)  -- Mark4で変更
                              where (d1, am) = compileU e2 (d + 1) env (d + 1)  -- Mark4で変更
                                    (d2, is) = compileR e1 env d1  -- Mark4で変更
@@ -340,7 +353,7 @@ compileA (ENum n) env = IntConst n  -- Mark4で変更
 -- Mark2で追加
 compileB :: CoreExpr -> TimCompilerEnv -> Int -> [Instruction] -> (Int, [Instruction])  -- Bスキーム  Mark3で変更
 compileB (EAp (EAp (EVar op) e1) e2) env d cont  -- Mark3で変更
-  = (d2, is2)  -- Mark3で変更
+  = (d3, is2)  -- Mark3で変更
     where
       i = case op of
           "+"  -> Op Add
@@ -837,9 +850,12 @@ nTerse = 3
 
 showCompiledCode :: String -> String
 showCompiledCode coreprg
-  = show codes
+  = iDisplay (iConcat [
+      iStr "Supercombinator definitions", iNewline, iNewline,
+      showSCDefns first_state, iNewline
+    ])
     where
-      (_, _, _, _, _, _, _, codes, _) = compile $ parse coreprg
+      first_state = compile $ parse coreprg
 
 showUsedSlotNumber :: [Instruction] -> Iseq
 showUsedSlotNumber []
@@ -1039,6 +1055,115 @@ ex_4_19 = "main = let x = 3 in x+x"
 ex_4_20 = "factorial n = if n 1 (n * factorial (n-1)) ; " ++
           "f x = x + x ; " ++
           "main = let arg = factorial 3 in f arg"
+ex_4_20_1 = "f x = let y = x "  ++
+                  "in let z =  y " ++
+                      "in z ; " ++
+            "main = f (3 + 5)"
+
+ex_4_20_2_1_1 = "x = 2 * 2 ; " ++
+                "main = 1 + x"  -- (EAp (EAp op e1) e2) の op が2項演算子で e2 が EVar v の場合 (e1 は ENum n)
+
+ex_4_20_2_1_2 = "x = 2 * 2 ; " ++
+                "y = 2 / 2 ; " ++
+                "main = y + x"  -- (EAp (EAp op e1) e2) の op が2項演算子で e2 が EVar v の場合 (e1 は EVar v)
+
+ex_4_20_2_1_3 = "x = 2 * 2 ; " ++
+                "inc n = n + 1 ; " ++
+                "main = (inc 2) + x"  -- (EAp (EAp op e1) e2) の op が2項演算子で e2 が EVar v の場合 (e1 は ENum n, EVar v 以外)
+
+ex_4_20_2_1_4 = "x = 2 * 2 ; " ++
+                "inc n = n + 1 ; " ++
+                "main = (K1 1 (inc 2)) + x"  -- (EAp (EAp op e1) e2) の op が2項演算子で e2 が EVar v の場合 (e1 は ENum n, EVar v 以外)
+
+ex_4_20_2_2_1 = "main = 1 + 2"  -- (EAp (EAp op e1) e2) の op が2項演算子で e2 が ENum n の場合 (e1 は ENum n)
+
+ex_4_20_2_2_2 = "y = 2 / 2 ; " ++
+                "main = y + 2"  -- (EAp (EAp op e1) e2) の op が2項演算子で e2 が ENum n の場合 (e1 は EVar v)
+
+ex_4_20_2_2_3 = "inc n = n + 1 ; " ++
+                "main = (inc 1) + 2"  -- (EAp (EAp op e1) e2) の op が2項演算子で e2 が ENum n の場合 (e1 は ENum n, EVar v 以外)
+
+ex_4_20_2_2_4 = "inc n = n + 1 ; " ++
+                "main = (K1 1 (inc 1)) + 2"  -- (EAp (EAp op e1) e2) の op が2項演算子で e2 が EVar v の場合 (e1 は ENum n, EVar v 以外)
+
+ex_4_20_2_3_1 = "inc n = n + 1 ; " ++
+                "main = 1 + (inc 2)"  -- (EAp (EAp op e1) e2) の op が2項演算子で e2 が EVar v, ENum n 以外の場合 (e1 は ENum n)
+
+ex_4_20_2_3_2 = "y = 2 / 2 ; " ++
+                "inc n = n + 1 ; " ++
+                "main = y + (inc 2)"  -- (EAp (EAp op e1) e2) の op が2項演算子で e2 が EVar v, ENum n 以外の場合 (e1 は EVar v)
+
+ex_4_20_2_3_3 = "inc n = n + 1 ; " ++
+                "main = (inc 1) + (inc 2)"  -- (EAp (EAp op e1) e2) の op が2項演算子で e2 が EVar v, ENum n 以外の場合 (e1 は ENum n, EVar v 以外)
+
+ex_4_20_2_3_4 = "inc n = n + 1 ; " ++
+                "main = (K1 1 (inc 1)) + (inc 2)"  -- (EAp (EAp op e1) e2) の op が2項演算子で e2 が EVar v, ENum n 以外の場合 (e1 は ENum n, EVar v 以外)
+
+ex_4_20_2_4_1 = "x = 2 * 2 ; " ++
+                "main = K1 1 x"  -- (EAp (EAp op e1) e2) の op が2項演算子以外で e2 が EVar v の場合 (e1 は ENum n)
+
+ex_4_20_2_4_2 = "x = 2 * 2 ; " ++
+                "y = 2 / 2 ; " ++
+                "main = K1 y x"  -- (EAp (EAp op e1) e2) の op が2項演算子以外で e2 が EVar v の場合 (e1 は EVar v)
+
+ex_4_20_2_4_3 = "x = 2 * 2 ; " ++
+                "inc n = n + 1 ; " ++
+                "main = K1 (inc 1) x"  -- (EAp (EAp op e1) e2) の op が2項演算子以外で e2 が EVar v の場合 (e1 は ENum n, EVar v 以外)
+
+ex_4_20_2_4_4 = "x = 2 * 2 ; " ++
+                "inc n = n + 1 ; " ++
+                "main = K1 (K1 1 (inc 1)) x"  -- (EAp (EAp op e1) e2) の op が2項演算子以外で e2 が EVar v の場合 (e1 は ENum n, EVar v 以外)
+
+ex_4_20_2_5_1 = "main = K1 1 2"  -- (EAp (EAp op e1) e2) の op が2項演算子以外で e2 が ENum n の場合 (e1 は ENum n)
+
+ex_4_20_2_5_2 = "y = 2 / 2 ; " ++
+                "main = K1 y 2"  -- (EAp (EAp op e1) e2) の op が2項演算子以外で e2 が ENum n の場合 (e1 は EVar v)
+
+ex_4_20_2_5_3 = "inc n = n + 1 ; " ++
+                "main = K1 (inc 2) 2"  -- (EAp (EAp op e1) e2) の op が2項演算子以外で e2 が ENum n の場合 (e1 は ENum n, EVar v 以外)
+
+ex_4_20_2_5_4 = "inc n = n + 1 ; " ++
+                "main = K1 (K1 1 (inc 2)) 2"  -- (EAp (EAp op e1) e2) の op が2項演算子以外で e2 が ENum n の場合 (e1 は ENum n, EVar v 以外)
+
+ex_4_20_2_6_1 = "inc n = n + 1 ; " ++
+                "main = K1 1 (inc 2)"  -- (EAp (EAp op e1) e2) の op が2項演算子以外で e2 が EVar v, ENum n 以外の場合 (e1 は ENum n)
+
+ex_4_20_2_6_2 = "y = 2 / 2 ; " ++
+                "inc n = n + 1 ; " ++
+                "main = K1 y (inc 2)"  -- (EAp (EAp op e1) e2) の op が2項演算子以外で e2 が EVar v, ENum n 以外の場合 (e1 は EVar v)
+
+ex_4_20_2_6_3 = "inc n = n + 1 ; " ++
+                "main = K1 (inc 1) (inc 2)"  -- (EAp (EAp op e1) e2) の op が2項演算子以外で e2 が EVar v, ENum n 以外の場合 (e1 は EVar v, ENum n 以外)
+ex_4_20_2_6_4 = "inc n = n + 1 ; " ++
+                "main = K1 (K1 1 (inc 1)) (inc 2)"  -- (EAp (EAp op e1) e2) の op が2項演算子以外で e2 が EVar v, ENum n 以外の場合 (e1 は EVar v, ENum n 以外)
+
+ex_4_20_2_7_1 = "inc n = n + 1 ; " ++
+                "main = K1 1 (K1 1 (inc 1))"  -- (EAp (EAp op e1) e2) の op が2項演算子以外で e2 が EVar v, ENum n 以外の場合 (e1 は ENum n)
+
+ex_4_20_2_7_2 = "y = 2 / 2 ; " ++
+                "inc n = n + 1 ; " ++
+                "main = K1 y (K1 1 (inc 1))"  -- (EAp (EAp op e1) e2) の op が2項演算子以外で e2 が EVar v, ENum n 以外の場合 (e1 は EVar v)
+
+ex_4_20_2_7_3 = "inc n = n + 1 ; " ++
+                "main = K1 (inc 2) (K1 1 (inc 1))"  -- (EAp (EAp op e1) e2) の op が2項演算子以外で e2 が EVar v, ENum n 以外の場合 (e1 は EVar v, ENum n 以外)
+
+ex_4_20_2_7_4 = "inc n = n + 1 ; " ++
+                "main = K1 (K1 2 (inc 2)) (K1 1 (inc 1))"  -- (EAp (EAp op e1) e2) の op が2項演算子以外で e2 が EVar v, ENum n 以外の場合 (e1 は EVar v, ENum n 以外)
+
+ex_4_20_2 = "f x = (x + x) + x ; " ++
+            "main = f (3 + 5)"
+ex_4_20_3 = "factorial n = if n 1 (n * factorial (n-1)) ; " ++
+            "f x = ((x + x) + x) + x ; " ++
+            "main = f (factorial 3)"
+ex_4_20_4 = "f x = g x ; " ++
+            "g x = h x ; " ++
+            "h x = i x ; " ++
+            "i x = j x ; " ++
+            "j x = k x ; " ++
+            "k x = l x ; " ++
+            "l x = x + x ; " ++
+            "main = f (3 + 5)"
+
 ex_4_21_1 = "pair x y f = f x y ; " ++
             "fst p = p K ; " ++
             "snd p = p K1 ; " ++
@@ -1054,6 +1179,13 @@ ex_4_21_3 = "pair x y f = f x y ; " ++
             "snd p = p K1 ; " ++
             "main = let w = if(2 * 3 > 8) (pair 2 3) (pair 3 2) " ++
                    "in (fst w) * (snd w)"
+ex_4_21_4 = "add a b = a+b ; " ++
+            "twice2 f x = f (f x) ; " ++
+            "g x = add (x*x) ; " ++
+            "main = twice2 (g 3) 4"
+ex_4_21_4_ = "add a b = a+b ; " ++
+             "g x = add (x*x) ; " ++
+             "main = twice (g 3) 4"
 --------------------------------
 -- テストプログラム (ここまで) --
 --------------------------------
